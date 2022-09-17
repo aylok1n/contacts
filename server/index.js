6858161b52e81fec357453aef2a5b53d59f44900
aylok1n <nikolya.blog@gmail.com>
2022-09-17 17:00:37 +0400

    Add json-server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const jsonServer = require('json-server');
+const auth = require('json-server-auth')
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.db = router.db
+server.use(middlewares);
+server.use(jsonServer.bodyParser)
+server.use(auth);
+server.use(router);
+server.listen(5000, () => {
+  console.log('JSON Server is running');
+});
